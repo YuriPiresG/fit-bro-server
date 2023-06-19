a0d78f71de02e57e6c93daaac6abad5f0760f676
Yuri Pires <yuripiresbr@gmail.com>
2023-06-19 13:23:35 -0300

    refactor: added user module to measure module and changed the create

--- src/measurements/measurements.module.ts
+++ src/measurements/measurements.module.ts
@@ -3,9 +3,10 @@ import { MeasurementsService } from './measurements.service';
 import { MeasurementsController } from './measurements.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Measurement } from './entities/measurement.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Measurement])],
+  imports: [TypeOrmModule.forFeature([Measurement]), UsersModule],
   controllers: [MeasurementsController],
   providers: [MeasurementsService],
   exports: [MeasurementsService],
--- src/measurements/measurements.service.ts
+++ src/measurements/measurements.service.ts
@@ -4,19 +4,50 @@ import { UpdateMeasurementDto } from './dto/update-measurement.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Measurement } from './entities/measurement.entity';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class MeasurementsService {
   constructor(
     @InjectRepository(Measurement)
     private measurementRepository: Repository<Measurement>,
+    private usersService: UsersService,
   ) {}
-  create(createMeasurementDto: CreateMeasurementDto) {
-    return this.measurementRepository.save(createMeasurementDto);
+  async create(createMeasurementDto: CreateMeasurementDto) {
+    const userFound = await this.usersService.findOne({
+      id: createMeasurementDto.userId,
+    });
+    if (!userFound || createMeasurementDto.userId !== userFound.id) {
+      throw new Error('Usuário não encontrado!');
+    }
+    const measurementCreated = this.measurementRepository.create();
+    measurementCreated.date = createMeasurementDto.date;
+    measurementCreated.weight = createMeasurementDto.weight;
+    measurementCreated.height = createMeasurementDto.height;
+    measurementCreated.bodyFat = createMeasurementDto.bodyFat;
+    measurementCreated.bicepsL = createMeasurementDto.bicepsL;
+    measurementCreated.bicepsR = createMeasurementDto.bicepsR;
+    measurementCreated.tricepsL = createMeasurementDto.tricepsL;
+    measurementCreated.tricepsR = createMeasurementDto.tricepsR;
+    measurementCreated.forearmL = createMeasurementDto.forearmL;
+    measurementCreated.forearmR = createMeasurementDto.forearmR;
+    measurementCreated.chest = createMeasurementDto.chest;
+    measurementCreated.waist = createMeasurementDto.waist;
+    measurementCreated.hips = createMeasurementDto.hips;
+    measurementCreated.thighL = createMeasurementDto.thighL;
+    measurementCreated.thighR = createMeasurementDto.thighR;
+    measurementCreated.calfL = createMeasurementDto.calfL;
+    measurementCreated.calfR = createMeasurementDto.calfR;
+    measurementCreated.back = createMeasurementDto.back;
+    measurementCreated.shoulderL = createMeasurementDto.shoulderL;
+    measurementCreated.shoulderR = createMeasurementDto.shoulderR;
+    measurementCreated.user = userFound as User;
+    return this.measurementRepository.save(measurementCreated);
   }
 
   findAll() {
-    return this.measurementRepository.find();
+    return this.measurementRepository.find({ relations: ['user'] });
   }
 
   findOne(id: number) {
